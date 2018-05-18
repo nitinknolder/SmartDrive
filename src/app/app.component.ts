0ca650239c69958a0f47919c8a6e1d6fe89da5d5
Nitin Arora <nitin.arora@gmail.com>
2018-05-18 18:46:06 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+import {AfterViewInit, Component, OnInit} from '@angular/core';
+// import * as $ from 'jquery';
+//
+// declare var $: any;
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent{
+
+  constructor() {}
+
+  // ngAfterViewInit() {
+  //   $(document).ready(function() {
+  //     $('.popup-top-anim').magnificPopup({
+  //       type: 'inline',
+  //       fixedContentPos: false,
+  //       fixedBgPos: true,
+  //       overflowY: 'auto',
+  //       closeBtnInside: true,
+  //       preloader: false,
+  //       midClick: true,
+  //       removalDelay: 300,
+  //       mainClass: 'my-mfp-zoom-in'
+  //     });
+  //   });
+  // }
+}
