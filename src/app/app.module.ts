bc0433e820443c3b62677268523d8e71e43e8ea0
Nitin Arora <nitin.arora@gmail.com>
2018-05-19 01:36:04 +0530

    added login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import {FormsModule} from '@angular/forms';
 import {RouterModule} from '@angular/router';
 import {routes} from './app.route';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RegisterComponent,
     HomeComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/app.route.ts
+++ src/app/app.route.ts
@@ -2,10 +2,13 @@ import {RouterModule, Routes} from '@angular/router';
 import {ModuleWithProviders} from '@angular/core';
 import {HomeComponent} from './home/home.component';
 import {RegisterComponent} from './register/register.component';
+import * as path from 'path';
+import {LoginComponent} from './login/login.component';
 
 export const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'register', component: RegisterComponent}
+  {path: 'register', component: RegisterComponent},
+  {path: 'login', component: LoginComponent}
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(routes);
